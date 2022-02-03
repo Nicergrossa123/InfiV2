a1dae5d6379392b9dde21d83e4b9a8aedaa20cf8
Vscriptde <Clickzy@gmx.de>
2022-02-03 13:31:13 +0100

    xxx

---
+++ Module/Telefon/App/Settings/Wallpaper/Wallpaper.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+
+namespace GVRP.Module.Telefon.App.Settings.Wallpaper
+{
+    public class Wallpaper : Loadable<uint>
+    {
+        [JsonProperty("id")]
+        public int Id
+        {
+            get;
+            set;
+        }
+        [JsonProperty("name")]
+        public string Name
+        {
+            get;
+            set;
+        }
+        [JsonProperty("file")]
+        public string File
+        {
+            get;
+            set;
+        }
+
+        public Wallpaper(int id, string name, string file)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.File = file;
+        }
+    }
+}
